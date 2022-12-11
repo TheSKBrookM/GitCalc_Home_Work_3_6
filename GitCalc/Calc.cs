55e9c44abe6fec7f8a07d690484994e68420ab59
Maxim Blinda <maximblinda@gmail.com>
2022-12-11 16:39:15 +0200

    add class

---
+++ GitCalc/Calc.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitCalc
+{
+    internal class Calc
+    {
+        public void Add(int a, int b)
+        {
+
+        }
+        public void Sub(int a, int b)
+        {
+
+        }
+        public void Div(int a, int b)
+        {
+
+        }
+        public void Mult(int a, int b)
+        {
+
+        }
+    }
+}
